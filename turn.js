97605c17aac48f40ba15dcd6762054513bd28d54
computernewb <rightowner@gmail.com>
2015-07-06 14:30:54 -0400

    Fix a serious vulnerability
    Fix a serious vulnerability that allows turn takeovers and sending remote QEMU commands.

--- turn.js
+++ turn.js
@@ -1,11 +1,16 @@
 var turnQueue = [];
 var activeTurn = false;
+var activeSocketId = module.exports.activeSocketId = null;
 var redis = require('./redis').emu();
 
 var TURN_TIME = 15000;
 
 module.exports.push = function(sockid) {
+	// Check if the socket ID is already in the queue
+	if (turnQueue.indexOf(sockid) != -1 || sockid == module.exports.activeSocketId)
+		return false;
   turnQueue.push(sockid);
+  return true;
 }
 
 module.exports.checkQueue = checkQueue;
@@ -17,10 +22,14 @@ function checkQueue(newReq) {
     var sockid = turnQueue.shift();
     io = io.in(sockid);
     io.emit('your-turn');
+	// Change the active socket ID that has control over the VM
+	module.exports.activeSocketId = sockid;
 
     setTimeout(function() {
       io.emit('lose-turn');
       activeTurn = false;
+	  // Set the active socket ID to null
+	  module.exports.activeSocketId = null;
       checkQueue(false);
     }, TURN_TIME);
 
